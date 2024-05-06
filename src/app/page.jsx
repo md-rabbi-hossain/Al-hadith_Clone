fdbd0eccc39cb78f51052fcf084b231d7adc0494
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2024-05-06 08:17:18 +0600

    Navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+
+export default function page() {
+  return (
+    <div className="w-full max-h-screen bg-white h-screen">
+      <Navbar />
+    </div>
+  );
+}
